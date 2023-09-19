32b182b5c4c25cad3a658bf8b13f8099d13e44f0
Lucas Galvao <lucas.engcomp@outlook.com>
2023-09-19 11:24:01 -0300

    :tada: Project created

---
+++ backend/src/main/java/com/lucasengcomp/challengepayment/ChallengePaymentApplication.java
@@ -0,0 +1,13 @@
+package com.lucasengcomp.challengepayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengePaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengePaymentApplication.class, args);
+	}
+
+}
