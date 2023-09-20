43c7c41f7d953dfa64074274461505ca62b2a703
Lucas Galvao <lucas.engcomp@outlook.com>
2023-09-20 00:33:10 -0300

    :test_tube:	`test` Added test PersonServiceImpl find and validations

--- backend/src/test/java/com/lucasengcomp/challengepayment/application/services/impl/PersonServiceImplTest.java
+++ backend/src/test/java/com/lucasengcomp/challengepayment/application/services/impl/PersonServiceImplTest.java
@@ -1,53 +1,95 @@
 package com.lucasengcomp.challengepayment.application.services.impl;
 
+
 import com.lucasengcomp.challengepayment.application.dto.PersonDTO;
-import com.lucasengcomp.challengepayment.domain.repositories.PersonRepository;
-import com.lucasengcomp.challengepayment.factory.PersonBuilder;
+import com.lucasengcomp.challengepayment.domain.exceptions.service.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
-
-@ExtendWith(SpringExtension.class)
+@Transactional
+@SpringBootTest
 class PersonServiceImplTest {
 
-    @Mock
-    private PersonRepository repository;
-
-    @InjectMocks
+    @Autowired
     private PersonServiceImpl service;
 
-    private PageImpl<PersonDTO> page;
-    private PersonDTO person;
+    private Long existingId = 1L;
+    private Long nonExistingId = 1000L;
 
-    @BeforeEach
-    void setUp() {
-        person = PersonBuilder.createPersonDTOValid();
-        page = new PageImpl<>(List.of(person));
 
-        Mockito.when(repository.findPageable(ArgumentMatchers.any())).thenReturn(page);
+    @Test
+    @DisplayName("Should return Person paged")
+    void findAllPaged() {
+        PageRequest pageRequest = PageRequest.of(0, 20);
+        Page<PersonDTO> result = service.findAllPaged(pageRequest);
+
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals("Joel Victor", result.getContent().get(0).getName());
+        Assertions.assertEquals("Eli Alves", result.getContent().get(1).getName());
+        Assertions.assertEquals("Lucas Galvao", result.getContent().get(2).getName());
+        Assertions.assertEquals("Matheus Carvalho", result.getContent().get(3).getName());
     }
 
     @Test
-    @DisplayName("Should retrieve a paged list of Persons")
-    void findAllPaged() {
-        Pageable pageable = PageRequest.of(0, 20);
-        Page<PersonDTO> result = service.findAllPaged(pageable);
-        Assertions.assertNotNull(result, "The returned page should not be null");
-        Assertions.assertNotNull(result);
-        Mockito.verify(repository, Mockito.times(1)).findPageable(pageable);
+    @DisplayName("Should return 3 people per page")
+    void findAllPaged_With3PeoplePerPage() {
+        PageRequest pageRequest = PageRequest.of(0, 3);
+        Page<PersonDTO> result = service.findAllPaged(pageRequest);
+
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals("Joel Victor", result.getContent().get(0).getName());
+        Assertions.assertEquals("Eli Alves", result.getContent().get(1).getName());
+        Assertions.assertEquals("Lucas Galvao", result.getContent().get(2).getName());
+    }
+
+    @Test
+    @DisplayName("Should return all people per page verify size")
+    void findAllPaged_ShouldReturn3PeoplePerPage() {
+        PageRequest pageRequest = PageRequest.of(0, 20);
+        Page<PersonDTO> result = service.findAllPaged(pageRequest);
+
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals(0, result.getNumber());
+        Assertions.assertEquals(20, result.getSize());
+        Assertions.assertEquals(4, result.getTotalElements());
+    }
+
+    @Test
+    @DisplayName("Should return page empty when page does not exist")
+    void findAllPaged_ShouldReturnEmptyPageWhenPageDoesNotExist() {
+        PageRequest pageRequest = PageRequest.of(50, 10);
+        Page<PersonDTO> result = service.findAllPaged(pageRequest);
+        Assertions.assertTrue(result.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Should search Person by id and verify fields")
+    void findById_verifyFields() {
+        PersonDTO personById = service.findById(existingId);
+
+        Assertions.assertNotNull(personById);
+        Assertions.assertEquals(1, personById.getId());
+        Assertions.assertEquals("Joel Victor", personById.getName());
+        Assertions.assertEquals(new BigDecimal("100.00"),
+                personById.getOrderValue().setScale(2, RoundingMode.HALF_UP));
+        Assertions.assertEquals(new BigDecimal("90.00"),
+                personById.getTotalToPay().setScale(2, RoundingMode.HALF_UP));
+    }
+
+    @Test
+    @DisplayName("Should throws exception ResourceNotFoundException when search inexistingId")
+    void findById_whenIdDoesNotExist() {
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            service.findById(nonExistingId);
+        });
     }
 }
