194a777969db56ee77a5b5e054317cf818db5cde
Lucas Galvao <lucas.engcomp@outlook.com>
2023-09-19 23:40:18 -0300

    :wrench: `chore` Added unity test

--- backend/src/main/java/com/lucasengcomp/challengepayment/application/dto/PersonDTO.java
+++ backend/src/main/java/com/lucasengcomp/challengepayment/application/dto/PersonDTO.java
@@ -2,12 +2,14 @@ package com.lucasengcomp.challengepayment.application.dto;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class PersonDTO {
