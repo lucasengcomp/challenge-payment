425a480c4828f94096b7df22a66691d06ef2e023
Lucas Galvao <lucas.engcomp@outlook.com>
2023-09-26 15:01:31 -0300

    :books:	`docs` Updated description schema sc_divideai

---
+++ backend/src/main/resources/db.migration/V008__alter_comments_schema.sql
@@ -0,0 +1,1 @@
+COMMENT ON SCHEMA sc_divideai IS 'Esquema de armazenamento de dados relacionados ao aplicativo de compartilhamento de despesas. Ele contém informações sobre pedidos, pessoas e transações financeiras.';
